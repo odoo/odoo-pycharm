ab66615ad624add960e8aeca8934295701e1219f
Florian Daloze <fda@odoo.com>
2025-09-02 11:28:22 +0200

    [IMP] copy script for exe from odoo-ls

---
+++ update_binaries.sh
@@ -0,0 +1,8 @@
+cp ../odoo-ls/server/target/aarch64-apple-darwin/release/odoo_ls_server src/main/resources/odools-binaries/darwin-arm64/odoo_ls_server
+cp ../odoo-ls/server/target/x86_64-apple-darwin/release/odoo_ls_server src/main/resources/odools-binaries/darwin-x64/odoo_ls_server
+cp ../odoo-ls/server/target/aarch64-unknown-linux-gnu/release/odoo_ls_server src/main/resources/odools-binaries/linux-arm64/odoo_ls_server
+cp ../odoo-ls/server/target/x86_64-unknown-linux-gnu/release/odoo_ls_server src/main/resources/odools-binaries/linux-x64/odoo_ls_server
+cp ../odoo-ls/server/target/aarch64-pc-windows-msvc/release/odoo_ls_server.exe src/main/resources/odools-binaries/win32-arm64/odoo_ls_server.exe
+cp ../odoo-ls/server/target/aarch64-pc-windows-msvc/release/odoo_ls_server.pdb src/main/resources/odools-binaries/win32-arm64/odoo_ls_server.pdb
+cp ../odoo-ls/server/target/x86_64-pc-windows-msvc/release/odoo_ls_server.exe src/main/resources/odools-binaries/win32-x64/odoo_ls_server.exe
+cp ../odoo-ls/server/target/x86_64-pc-windows-msvc/release/odoo_ls_server.pdb src/main/resources/odools-binaries/win32-x64/odoo_ls_server.pdb
